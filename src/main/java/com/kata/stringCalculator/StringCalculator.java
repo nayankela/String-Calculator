6bb56d60934dd5494816017b79f09ba309df61b3
nayankela <nayankela09@gmail.com>
2020-12-16 16:32:42 +0530

    first commit

--- src/main/java/com/kata/stringCalculator/StringCalculator.java
+++ src/main/java/com/kata/stringCalculator/StringCalculator.java
@@ -3,6 +3,12 @@ package com.kata.stringCalculator;
 public class StringCalculator {
 
 	public static int add(String numbers) {
+	if(numbers.isEmpty()) {
 		return 0;
+	}else {
+		
+		return Integer.parseInt(numbers);
+	}
+
 	}
 }
--- src/test/java/com/kata/stringCalculatorTest/StringCalculatorTest.java
+++ src/test/java/com/kata/stringCalculatorTest/StringCalculatorTest.java
@@ -1,8 +1,6 @@
 package com.kata.stringCalculatorTest;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Test;
 
 import com.kata.stringCalculator.StringCalculator;
@@ -11,6 +9,12 @@ public class StringCalculatorTest {
 
 	@Test
 	public void sumsEmptyStringTo0() {
-		assertEquals(StringCalculator.add(""), -1);
+		assertEquals(StringCalculator.add(""), 0);
+	}
+
+	@Test
+	public void sumsSingleNumberToItself() {
+		assertEquals(StringCalculator.add("5"), 5);
+		assertEquals(StringCalculator.add("42"), 42);
 	}
 }
